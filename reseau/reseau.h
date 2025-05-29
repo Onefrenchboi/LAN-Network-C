26ad18a446a273a93a109506773383fe447be001
m.faucon <m.faucon@etu.unistra.fr>
2025-05-30 00:11:58 +0200

    debut de struc reseau ? pas sur de moi la

--- reseau/reseau.h
+++ reseau/reseau.h
@@ -5,6 +5,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdio.h>
+#include "equipement.h"
+
+
+
+typedef struct reseau{
+    int nb_equipements;
+    int nb_liens;
+    equipement** equipements; //ptr de ptr parce qu'on va prendre un ptr sur chacune des trucs pour toucher dessus
+} reseau;
+
 
 
 
